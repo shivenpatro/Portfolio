7dea226f2abc845625b10cfc58d6402730918de8
shivenpatro <142834696+shivenpatro@users.noreply.github.com>
2025-04-05 21:24:29 +0000

    Deploying to gh-pages from @ shivenpatro/Portfolio@6c28ca88405903fe3fec38d34df3b2d4f1ae6c2b 🚀

---
+++ _next/static/chunks/pages/_app-af040b30c5c43623.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{4427:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(9110)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(4427),n(4112))),_N_E=_.O()}]);
